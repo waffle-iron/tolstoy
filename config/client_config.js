749cfb57b611436245dcc9b29d6ee5abbd0eaac0
Mihail Paley <undeadlol1@mail.ru>
2016-11-05 07:19:08 +0500

    (client_config) enter different values

--- config/client_config.js
+++ config/client_config.js
@@ -1,37 +1,34 @@
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
-export const APP_NAME = 'Голос' // 'Steemit'
-export const APP_NAME_UPPERCASE = 'Golos' // 'STEEMIT'
-export const APP_ICON = 'golos' // 'steem'
-export const APP_URL = 'golos.io' // 'steemit.com'
-export const OWNERSHIP_TOKEN = 'Голос' // 'Steem'
+export const APP_NAME = 'Steemit' // 'Steemit'
+export const APP_NAME_UPPERCASE = 'STEEMIT' // 'STEEMIT'
+export const APP_ICON = 'steem' // 'steem'
+export const APP_URL = 'steemit.com' // 'steemit.com'
+export const OWNERSHIP_TOKEN = 'Steem' // 'Steem'
 // sometimes it's impossible to use html tags to style coin name, hence usage of _UPPERCASE modifier
-export const OWNERSHIP_TOKEN_UPPERCASE = 'ГОЛОС' // 'STEEM'
-export const INVEST_TOKEN = 'Сила Голоса' // 'Steem Power'
-export const INVEST_TOKEN_UPPERCASE = 'СИЛА ГОЛОСА' // 'STEEM POWER'
-export const INVEST_TOKEN_SHORT = 'СГ' // 'SP'
-export const DEBT_TOKEN = 'Золотой' // 'STEEM DOLLAR'
-// export const DEBT_TOKEN_UPPERCASE = 'Golos Backed Gold' // 'STEEM DOLLAR'
-// export const DEBT_TOKEN_SHORT = 'GBG' // 'SD'
-export const CURRENCY_SIGN = '₽≈' // '$'
+export const OWNERSHIP_TOKEN_UPPERCASE = 'STEEM' // 'STEEM'
+export const INVEST_TOKEN = 'Steem Power' // 'Steem Power'
+export const INVEST_TOKEN_UPPERCASE = 'STEEM POWER' // 'STEEM POWER'
+export const INVEST_TOKEN_SHORT = 'SP' // 'SP'
+export const DEBT_TOKEN = 'STEEM DOLLAR' // 'STEEM DOLLAR'
+export const CURRENCY_SIGN = '$' // '$'
 export const LANDING_PAGE_URL = 'https://golos.io/ico'
 export const WHITEPAPER_URL = 'https://golos.io/ru--golos/@golos/golos-russkoyazychnaya-socialno-mediinaya-blokchein-platforma'
-export const SEGMENT_ANALYTICS_KEY = 'F7tldQJxt491gXYqDGi5TkTT4wFpSPps'
+export const SEGMENT_ANALYTICS_KEY = ''
 // used on application internationalization bootstrap
 export const DEFAULT_LANGUAGE = 'ru' // 'en'
 
-// these are dealing with asset types, not displaying to client, rather sending data over websocket
+export const DEFAULT_CURRENCY = 'USD'
+export const ALLOWED_CURRENCIES = ['RUB', 'UAH', 'BYN', 'USD', 'EUR', 'CNY', 'GEL', 'KZT']
+export const FRACTION_DIGITS = 2 // default amount of decimal digits
+export const FRACTION_DIGITS_MARKET = 3 // accurate amount of deciaml digits (example: used in market)
 
+// these are dealing with asset types, not displaying to client, rather sending data over websocket
 export const OWNERSHIP_TICKER = 'GOLOS'
 export const INVEST_TICKER = 'GP'
 export const VEST_TICKER = 'GESTS'
 export const DEBT_TICKER = 'GBG'
 export const DEBT_TOKEN_SHORT = 'GBG' // 'SD'
 
-export const DEFAULT_CURRENCY = 'RUB'
-export const ALLOWED_CURRENCIES = ['RUB', 'UAH', 'BYN', 'USD', 'EUR', 'CNY', 'GEL', 'KZT']
-export const FRACTION_DIGITS = 2 // default amount of decimal digits
-export const FRACTION_DIGITS_MARKET = 3 // accurate amount of deciaml digits (example: used in market)
-
-export const SITE_DESCRIPTION = 'Голос - социальная сеть, построенная на публичном блокчейне. Вознаграждение пользователей осуществляется за счет дополнительной эмиссии токенов. При этом распределением токенов управляют текущие пользователи сети через голосование за лучший контент.'
+export const SITE_DESCRIPTION = 'Steemit is a social media platform where everyone gets paid for creating and curating content. It leverages a robust digital points system, called Steem, that supports real value for digital rewards through market price discovery and liquidity'
 
 export const FIRST_DATE = new Date(Date.UTC(2016, 7, 1)); //1 september
